03dc22a9d3d0caf2138e190b051773a47e0705a7
Chia-Wei Wu <cwwu@umich.edu>
2017-11-02 14:27:40 -0700

    Complete phase 1

--- js_intro_exercises/phase_1_arrays.js
+++ js_intro_exercises/phase_1_arrays.js
@@ -7,3 +7,48 @@ Array.prototype.uniq = function uniq(){
   });
   return arr;
 };
+
+Array.prototype.twoSum = function twoSum(){
+  let pairs = [];
+
+  for(let i = 0; i < this.length; i++){
+    let j = i + 1;
+
+    while (j < this.length) {
+      if (this[i] + this[j] === 0) {
+        pairs.push([i, j]);
+      }
+      j++;
+    }
+  }
+
+  return pairs;
+};
+
+Array.prototype.transpose = function transpose(){
+  let grid = [];
+
+  for(let i = 0; i < this.length; i++){
+    let j = 0;
+
+    let mini = [];
+    while (j < this.length){
+      mini.push(this[j][i]);
+      j++;
+    }
+    grid.push(mini);
+
+
+  }
+  return grid;
+};
+
+// [[1,2,3],
+//  [4,5,6],
+//  [7,8,9]]
+//
+// WE WANT THIS:
+//
+// [[1,4,7],
+//  [2,5,8],
+//  [3,6,9]]
